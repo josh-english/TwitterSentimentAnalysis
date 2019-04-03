9f0800e7ca594152bc0bcd2ec41fb7d2aa015b2b
Douglas Schonholtz <schonholtz.d@husky.neu.edu>
2019-04-03 15:07:17 -0400

    fixed db coordinates still garbage

--- FlaskApp/app/twitter_visualization/forms.py
+++ FlaskApp/app/twitter_visualization/forms.py
@@ -1,8 +1,8 @@
 # Import Form and RecaptchaField (optional)
-from flask_wtf import Form # , RecaptchaField
+from flask_wtf import Form  # , RecaptchaField
 
 # Import Form elements such as TextField and BooleanField (optional)
-from wtforms import TextField, PasswordField # BooleanField
+from wtforms import TextField, PasswordField  # BooleanField
 
 # Import Form validators
 from wtforms.validators import Required, Email, EqualTo
@@ -11,7 +11,7 @@ from wtforms.validators import Required, Email, EqualTo
 # Define the login form (WTForms)
 
 class LoginForm(Form):
-    email    = TextField('Email Address', [Email(),
-                Required(message='Forgot your email address?')])
+    email = TextField('Email Address', [Email(),
+                                        Required(message='Forgot your email address?')])
     password = PasswordField('Password', [
-                Required(message='Must provide a password. ;-)')])
+        Required(message='Must provide a password. ;-)')])
