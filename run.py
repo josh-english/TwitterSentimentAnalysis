bdfcf77403bc4c367898485c89639cf5c745f3aa
Douglas Schonholtz <schonholtz.d@husky.neu.edu>
2019-04-19 21:20:20 -0400

    restoring run file

--- run.py
+++ run.py
@@ -1,11 +1,8 @@
-#!/usr/bin/env python
 # Run a test server.
 from app import app
 import os
 
 if __name__ == '__main__':
     app.debug = True
-    # port = int(os.environ.get("PORT", 5000))
-    app.run()
-
-# adding pointless comment for push to heroku
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
